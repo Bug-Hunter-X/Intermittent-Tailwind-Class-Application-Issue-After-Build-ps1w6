a80c30716e59dad918a7daaa62787252228a8b9d
BugHunter-X <askzenai@gmail.com>
2024-12-17 06:42:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Some Tailwind classes are not applied even after running npm run build
+<div class="bg-red-500 p-4">
+  <p>This text should be red and have padding, but sometimes it isn't.</p>
+</div>
+```
